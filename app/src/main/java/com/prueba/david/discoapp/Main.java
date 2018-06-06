441f5d683f4e14d64bb02346432c1b6f2ef447df
EderV <EderV@github.com>
2018-06-06 10:43:24 +0200

    p

--- app/src/main/java/com/prueba/david/discoapp/Main.java
+++ app/src/main/java/com/prueba/david/discoapp/Main.java
@@ -10,10 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Main extends AppCompatActivity {
-    //  Prueba
-    int i = 0;
-    int j = 1;
-    int a = j;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
